232126076b1cf23b386526d843a8f2505802c019
Huitre <gohin.j@gmail.com>
2015-01-17 22:32:58 +0100

    Landing login page

--- index.js
+++ index.js
@@ -51,7 +51,7 @@ app.use(function(req, res, next) {
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/public/'));
 
 /* passport for login/session */
 
