08f767e5fbf37c6d226016e354a36e48db7e524f
unknown <joffrey.gohin@PARPCD12.publications-metro.fr>
2015-07-15 17:57:48 +0200

    WIP badges

--- application/bo/feed.js
+++ application/bo/feed.js
@@ -5,64 +5,53 @@
 var Db = require('../models'),
     Person = require('../bo/person'),
     crypto = require('crypto'),
+    events = require('events'),
     console = require('console-plus');
 
-var FeedModel = (function () {
+var FeedModel = function () {}
 
-  /*
-   * Return lastest posts and comments
-   * of a user and his friends
-   *
-   *
-   * @params User || id
-   * @params callback
-   */
-  this.getFeed = function (User, done) {
-    var publicy, UserId;
-    
-    if (typeof User == "object") {
-      publicy = false;
-      UserId = User.id
-    } else {
-      UserId = User;
-    }
+FeedModel.prototype = Object.create(events.EventEmitter.prototype);
 
-    Person.getFriendsIdList(UserId).then(function (friends) {
-      var friendsId = []
-      friends.map(function (friend) {
-        friendsId.push(friend.FriendId);
-      })
-      friendsId.push(UserId)
-      Db.Post.findAll({
-        where : {
-          PersonId: friendsId
-        },
-        order : '"Post"."updatedAt" DESC',
-        limit: 30,
+
+/*
+ * Return lastest posts and comments
+ * of a user and his friends
+ *
+ *
+ * @params User || id
+ * @params callback
+ */
+FeedModel.prototype.getFeed = function (User, done) {
+  var publicy, UserId;
+  
+  if (typeof User == "object") {
+    publicy = false;
+    UserId = User.id
+  } else {
+    UserId = User;
+  }
+
+  Person.getFriendsIdList(UserId).then(function (friends) {
+    var friendsId = []
+    friends.map(function (friend) {
+      friendsId.push(friend.FriendId);
+    })
+    friendsId.push(UserId)
+    Db.Post.findAll({
+      where : {
+        PersonId: friendsId
+      },
+      order : '"Post"."updatedAt" DESC',
+      limit: 30,
+      include : [{
+        model: Db.Comment,
         include : [{
-          model: Db.Comment,
-          include : [{
-            model: Db.Person,
-            attributes : ['id'],
-            include : [{
-              model : Db.PersonDetails,
-              attributes : ['name', 'firstname']
-            }, {
-              model : Db.Avatar,
-              attributes : ['id'],
-              include : [{
-                model: Db.Image,
-                attributes : ['resource']
-              }]
-            }]
-          }]
-        },{
           model: Db.Person,
           attributes : ['id'],
           include : [{
             model : Db.PersonDetails,
             attributes : ['name', 'firstname']
-          },{
+          }, {
             model : Db.Avatar,
             attributes : ['id'],
             include : [{
@@ -71,123 +60,136 @@ var FeedModel = (function () {
             }]
           }]
         }]
-      }).then(function (posts) {
-        result = {
-          post : posts
-        }
-        if (!publicy)
-          result.profile = User;
-        done(null, result);
-      }).catch (function (e) {
-        done(e)
-      })
-    })
-  }
-
-  this.findOnePost = function (id, done) {
-    Db.Post.find({
-        where : {
-          id: id
-        },
-        include : [{
-          model: Db.Comment,
-          include : {
-            model: Db.Person,
-            attributes : ['id'],
-            include : [{
-              model : Db.PersonDetails,
-              attributes : ['name', 'firstname']
-            }, {
-              model : Db.Avatar,
-              include : [{
-                model: Db.Image
-              }]
-            }]
-          }
-        },{
-          model: Db.Person,
-          attributes : ['id'],
-          include : [{
-            model : Db.PersonDetails,
-            attributes : ['name', 'firstname']
-          },{
-            model : Db.Image
-          }]
-        }]
-      }).then(function (post) {
-        done(null, post)
-      }).catch(done);
-  }
-
-  this.findOneComment = function (id, done) {
-    Db.Comment.find({
-      where : {
-        id : id
-      },  
-      include : {
+      },{
         model: Db.Person,
         attributes : ['id'],
         include : [{
           model : Db.PersonDetails,
           attributes : ['name', 'firstname']
-        }, {
+        },{
           model : Db.Avatar,
+          attributes : ['id'],
           include : [{
-            model: Db.Image
+            model: Db.Image,
+            attributes : ['resource']
           }]
         }]
+      }]
+    }).then(function (posts) {
+      result = {
+        post : posts
       }
+      if (!publicy)
+        result.profile = User;
+      done(null, result);
+    }).catch (function (e) {
+      done(e)
+    })
+  })
+}
+
+FeedModel.prototype.findOnePost = function (id, done) {
+  Db.Post.find({
+      where : {
+        id: id
+      },
+      include : [{
+        model: Db.Comment,
+        include : {
+          model: Db.Person,
+          attributes : ['id'],
+          include : [{
+            model : Db.PersonDetails,
+            attributes : ['name', 'firstname']
+          }, {
+            model : Db.Avatar,
+            include : [{
+              model: Db.Image
+            }]
+          }]
+        }
+      },{
+        model: Db.Person,
+        attributes : ['id'],
+        include : [{
+          model : Db.PersonDetails,
+          attributes : ['name', 'firstname']
+        },{
+          model : Db.Image
+        }]
+      }]
     }).then(function (post) {
       done(null, post)
     }).catch(done);
-  }
+}
 
-  this.addPost = function (User, content, done) {
-    var UserId, bo = this;
-    
-    UserId = User.id || User;
-    // TODO parse content toget pictures/links/video
-    Db.Post.create({
-      content : {text: content},
-      PersonId: UserId
-    }).then(function (post) {
-      bo.findOnePost(post.id, done);
-    }).catch(done);
-  }
+FeedModel.prototype.findOneComment = function (id, done) {
+  Db.Comment.find({
+    where : {
+      id : id
+    },  
+    include : {
+      model: Db.Person,
+      attributes : ['id'],
+      include : [{
+        model : Db.PersonDetails,
+        attributes : ['name', 'firstname']
+      }, {
+        model : Db.Avatar,
+        include : [{
+          model: Db.Image
+        }]
+      }]
+    }
+  }).then(function (post) {
+    done(null, post)
+  }).catch(done);
+}
 
-  this.deletePost = function (User, postId, done) {
-    var UserId, bo = this;
-    
-    UserId = User.id || User;
-    
-    Db.Post.destroy({
-      PostId: postId,
-      PersonId: UserId
-    }).then(function (post) {
-      done(null, true)
-    }).catch(done);
-  }
+FeedModel.prototype.addPost = function (User, content, done) {
+  var UserId, bo = this;
+  
+  UserId = User.id || User;
+  // TODO parse content toget pictures/links/video
+  Db.Post.create({
+    content : {text: content},
+    PersonId: UserId
+  }).then(function (post) {
+    bo.findOnePost(post.id, done);
+  }).catch(done);
+}
 
-  this.addComment = function (User, content, postId, done) {
-    var publicy, UserId, bo = this;
-    
-    if (typeof User == "object")
-      publicy = false;
-    
-    UserId = User.id || User;
+FeedModel.prototype.deletePost = function (User, postId, done) {
+  var UserId, bo = this;
+  
+  UserId = User.id || User;
+  
+  Db.Post.destroy({
+    PostId: postId,
+    PersonId: UserId
+  }).then(function (post) {
+    done(null, true)
+  }).catch(done);
+}
 
-    Db.Comment.create({
-      content: {text: content},
-      PersonId: UserId,
-      PostId: postId
-    }).then(function (comment) {
-      bo.findOneComment(comment.id, done);
-    }).catch(done);
-  }
+FeedModel.prototype.addComment = function (User, content, postId, done) {
+  var publicy, UserId, bo = this;
+  
+  if (typeof User == "object")
+    publicy = false;
+  
+  UserId = User.id || User;
 
-  return this;
-})()
+  Db.Comment.create({
+    content: {text: content},
+    PersonId: UserId,
+    PostId: postId
+  }).then(function (comment) {
+    bo.findOneComment(comment.id, done);
+  }).catch(done);
+}
+
 
 if (typeof module !== 'undefined') {
-  module.exports = FeedModel;
+  module.exports = new FeedModel;
 }
--- application/bo/person.js
+++ application/bo/person.js
@@ -5,252 +5,254 @@
 var Db = require('../models'),
     crypto = require('crypto'),
     Promise = require("bluebird"),
+    events = require('events'),
     console = require('console-plus');
 
-var PersonModel = (function () {
+var PersonModel = function () {}
+
+PersonModel.prototype = Object.create(events.EventEmitter.prototype);
+  
+PersonModel.prototype.populateFriends = function (rows) {
+  var friends = [],
+      populate = function (row) {
+        friends.push({
+          id : row.FriendId,
+          type : row.type,
+          gender : row.Friend.PersonDetails[0].gender,
+          age : row.Friend.PersonDetails[0].age,
+          updatedAt : row.updatedAt,
+          firstname : row.Friend.PersonDetails[0].firstname,
+          name : row.Friend.PersonDetails[0].name,
+          avatar : row.Friend.Avatar.Image
+        });
+      }
+
+  if (rows.hasOwnProperty('length')) 
+    rows.map(populate)
+  else 
+    populate(rows)
+  
+  return friends;
+}
+
+PersonModel.prototype.authFindOrCreate = function (profile, done) {
+  var where = {},
+      md5 = crypto.createHash('md5');
+  
+  try {
+    where.email = profile.emails[0].value
+  } catch (e) {
+    where.email = profile.email || null;
+  }
+  where.password = profile.password || null;
+  where.password = md5.update(where.password).digest('hex');
+
+  if (profile.authOrign == 'google')
+    where.gid = profile.id;
+  if (profile.authOrign == 'facebook')
+    where.fbid = profile.id;
+
+  Db.Person.findOrCreate({
+    where : where,
+    include : [{
+      model: Db.PersonDetails 
+    }]
+  }).spread(function (User) {
+    if (!User.PersonDetails || User.PersonDetails.length < 1) {
+      var details = Db.PersonDetails.build({
+        type : 'owner',
+        name : profile.family_name || profile.lastName,
+        firstname : profile.given_name || profile.firstName
+      })
+      User.addPersonDetails(details);
+    }
+    return done(null, User);
+  }).catch(function (err) {
+    return done(err, false);
+  });
+}
+
+/*
+ * @return Promise
+ */
+PersonModel.prototype.getAll = function (id, done) {
+  return Db.Person.findAll({
+        attributes: ['Person.*', 'PeopleFriends.*', [Db.sequelize.fn('COUNT', 'PeopleFriends.id'), 'FriendsCount']],
+        include: [Db.PeopleFriend],
+        group : ['Person.id', 'PeopleFriends.id']
+      })
+}
+
+/*
+ * @return Promise
+ */
+PersonModel.prototype.getOne = function (id, done) {
+  return Db.Person.find({ 
+    where : {
+      id : id
+    },
+    attributes : ['email', 'id'],
+    include : [{
+      model: Db.PersonDetails 
+    }]
+  })
+}
+
+/*
+ * @return Promise
+ */
+PersonModel.prototype.getFriends = function (UserId, isFriend, refused) {
   var self = this;
 
-  this.populateFriends = function (rows) {
-    var friends = [],
-        populate = function (row) {
-          friends.push({
-            id : row.FriendId,
-            type : row.type,
-            gender : row.Friend.PersonDetails[0].gender,
-            age : row.Friend.PersonDetails[0].age,
-            updatedAt : row.updatedAt,
-            firstname : row.Friend.PersonDetails[0].firstname,
-            name : row.Friend.PersonDetails[0].name,
-            avatar : row.Friend.Avatar.Image
-          });
-        }
-
-    if (rows.hasOwnProperty('length')) 
-      rows.map(populate)
-    else 
-      populate(rows)
-    
-    return friends;
-  }
-
-  this.authFindOrCreate = function (profile, done) {
-    var where = {},
-        md5 = crypto.createHash('md5');
-    
-    try {
-      where.email = profile.emails[0].value
-    } catch (e) {
-      where.email = profile.email || null;
-    }
-    where.password = profile.password || null;
-    where.password = md5.update(where.password).digest('hex');
-
-    if (profile.authOrign == 'google')
-      where.gid = profile.id;
-    if (profile.authOrign == 'facebook')
-      where.fbid = profile.id;
-
-    Db.Person.findOrCreate({
-      where : where,
+  refused = refused || false;
+  return new Promise(function (fulfill, reject){
+    Db.PeopleFriend.findAll({
+      where : {PersonId: UserId, confirmed: isFriend, refused : refused},
       include : [{
-        model: Db.PersonDetails 
-      }]
-    }).spread(function (User) {
-      if (!User.PersonDetails || User.PersonDetails.length < 1) {
-        var details = Db.PersonDetails.build({
-          type : 'owner',
-          name : profile.family_name || profile.lastName,
-          firstname : profile.given_name || profile.firstName
-        })
-        User.addPersonDetails(details);
-      }
-      return done(null, User);
-    }).catch(function (err) {
-      return done(err, false);
-    });
-  }
-  
-  /*
-   * @return Promise
-   */
-  this.getAll = function (id, done) {
-    return Db.Person.findAll({
-          attributes: ['Person.*', 'PeopleFriends.*', [Db.sequelize.fn('COUNT', 'PeopleFriends.id'), 'FriendsCount']],
-          include: [Db.PeopleFriend],
-          group : ['Person.id', 'PeopleFriends.id']
-        })
-  }
-
-  /*
-   * @return Promise
-   */
-  this.getOne = function (id, done) {
-    return Db.Person.find({ 
-      where : {
-        id : id
-      },
-      attributes : ['email', 'id'],
-      include : [{
-        model: Db.PersonDetails 
-      }]
-    })
-  }
-
-  /*
-   * @return Promise
-   */
-  this.getFriends = function (UserId, isFriend, refused) {
-    refused = refused || false;
-    return new Promise(function (fulfill, reject){
-      Db.PeopleFriend.findAll({
-        where : {PersonId: UserId, confirmed: isFriend, refused : refused},
+        model: Db.Person,
+        as : 'Friend',
+        attributes : ['id'],
         include : [{
-          model: Db.Person,
-          as : 'Friend',
-          attributes : ['id'],
+          model : Db.PersonDetails,
+        }, {
+          model : Db.Avatar,
           include : [{
-            model : Db.PersonDetails,
-          }, {
-            model : Db.Avatar,
-            include : [{
-              model: Db.Image
-            }]
+            model: Db.Image
           }]
         }]
-      }).then(function (rows) {
-        var friends = self.populateFriends(rows)
-        fulfill(friends)
-      }).catch(function (e) {
-        reject(e)
-      })
-    }) 
-  }
-
-  /*
-   * @return Promise
-   */
-  this.getFriendsIdList = function (UserId) {
-    return Db.PeopleFriend.findAll({
-      where : {PersonId: UserId, confirmed: true},
+      }]
+    }).then(function (rows) {
+      var friends = self.populateFriends(rows)
+      fulfill(friends)
+    }).catch(function (e) {
+      reject(e)
     })
-  }
+  }) 
+}
 
-  /*
-   * @return Promise
-   */
-  this.findByName = function (name) {
-    return Db.PersonDetails.findAll({
-      where : {
-        $or : [
-          {name : { ilike : '%' + name + '%' }},
-          {firstname : { ilike : '%' + name + '%'}}
-        ]
-      }
-    })
-  }
+/*
+ * @return Promise
+ */
+PersonModel.prototype.getFriendsIdList = function (UserId) {
+  return Db.PeopleFriend.findAll({
+    where : {PersonId: UserId, confirmed: true},
+  })
+}
 
-  this.request = {}
+/*
+ * @return Promise
+ */
+PersonModel.prototype.findByName = function (name) {
+  return Db.PersonDetails.findAll({
+    where : {
+      $or : [
+        {name : { ilike : '%' + name + '%' }},
+        {firstname : { ilike : '%' + name + '%'}}
+      ]
+    }
+  })
+}
 
-  /*
-   * @return Promise
-   */
-  this.request.get = function (userId) {
-    return self.getFriends(userId, false, false);
-  }
+PersonModel.prototype.request = {}
 
-  /*
-   * @return Promise
-   */
-  this.request.post = function (userId, friendId) {
-    return Db.PeopleFriend.findOrCreate({
+/*
+ * @return Promise
+ */
+PersonModel.prototype.request.get = function (userId) {
+  return this.getFriends(userId, false, false);
+}
+
+/*
+ * @return Promise
+ */
+PersonModel.prototype.request.post = function (userId, friendId) {
+  return Db.PeopleFriend.findOrCreate({
+    where : {
+      FriendId : friendId,
+      PersonId : userId
+    },
+    defaults : {
+      type : 'hamster',
+      FriendId : friendId,
+      PersonId : userId,
+      confirmed : false,
+      refused : false
+    }
+  })
+}
+
+/*
+ * @return Promise
+ */
+PersonModel.prototype.accept = function (userId, friendId) {
+  var self = this;
+
+  return new Promise(function (fulfill, reject) {
+    Db.PeopleFriend.find({
       where : {
         FriendId : friendId,
         PersonId : userId
       },
-      defaults : {
-        type : 'hamster',
-        FriendId : friendId,
-        PersonId : userId,
-        confirmed : false,
-        refused : false
-      }
-    })
-  }
-
-  /*
-   * @return Promise
-   */
-  this.accept = function (userId, friendId) {
-    return new Promise(function (fulfill, reject) {
-      Db.PeopleFriend.find({
-        where : {
-          FriendId : friendId,
-          PersonId : userId
-        },
+      include : [{
+        model: Db.Person,
+        as : 'Friend',
+        attributes : ['id'],
         include : [{
-          model: Db.Person,
-          as : 'Friend',
-          attributes : ['id'],
+          model : Db.PersonDetails,
+        }, {
+          model : Db.Avatar,
           include : [{
-            model : Db.PersonDetails,
-          }, {
-            model : Db.Avatar,
-            include : [{
-              model: Db.Image
-            }]
+            model: Db.Image
           }]
         }]
-      }).then(function (friend) {
-        friend.confirmed = true;
-        friend.save().then(function (row) {
-          fulfill(self.populateFriends(row))
-        })
-      }).catch(function (e) {
-        reject(new Error({msg : 'not found'}))
+      }]
+    }).then(function (friend) {
+      friend.confirmed = true;
+      friend.save().then(function (row) {
+        fulfill(self.populateFriends(row))
       })
+    }).catch(function (e) {
+      reject(new Error({msg : 'not found'}))
     })
-  }
+  })
+}
 
-  this.remove = function (userId, friendId) {
-    return new Promise(function (fulfill, reject) {
-      Db.PeopleFriend.find({
-        where : {
-          FriendId : friendId,
-          PersonId : userId
-        }
-      }).then(function (friend) {
-        friend.destroy().then(function () {
-          fulfill({destroy : true})
-        })
-      }).catch(function (e) {
-        reject(new Error({msg : 'not found'}))
+PersonModel.prototype.remove = function (userId, friendId) {
+  return new Promise(function (fulfill, reject) {
+    Db.PeopleFriend.find({
+      where : {
+        FriendId : friendId,
+        PersonId : userId
+      }
+    }).then(function (friend) {
+      friend.destroy().then(function () {
+        fulfill({destroy : true})
       })
+    }).catch(function (e) {
+      reject(new Error({msg : 'not found'}))
     })
-  }
+  })
+}
 
-  this.refuse = function (userId, friendId) {
-    return new Promise(function (fulfill, reject) {
-      Db.PeopleFriend.find({
-        where : {
-          FriendId : friendId,
-          PersonId : userId
-        }
-      }).then(function (friend) {
-        friend.confirmed = false;
-        friend.refused = true;
-        friend.save().then(function () {
-          fulfill({refused : true})
-        })
-      }).catch(function (e) {
-        reject(new Error({msg : 'not found'}))
+PersonModel.prototype.refuse = function (userId, friendId) {
+  return new Promise(function (fulfill, reject) {
+    Db.PeopleFriend.find({
+      where : {
+        FriendId : friendId,
+        PersonId : userId
+      }
+    }).then(function (friend) {
+      friend.confirmed = false;
+      friend.refused = true;
+      friend.save().then(function () {
+        fulfill({refused : true})
       })
+    }).catch(function (e) {
+      reject(new Error({msg : 'not found'}))
     })
-  }
-
-
-  return this;
-})()
+  })
+}
 
 if (typeof module !== 'undefined') {
-  module.exports = PersonModel;
+  module.exports = new PersonModel;
 }
